61ab7d72f8d66ca0da1b2d3242c3d207f481086b
tylermorrisford <tylerford@gmail.com>
2019-07-23 01:16:12 -0600

    added generate numbers function, jq for target number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,6 +11,14 @@ chooseNumbers();
 
 // generate random numbers: target between 19-120; crystals between 1-12
 function chooseNumbers() {
+target = getRandomRange(19,120);
+$('#targetNumber').text(target);
+console.log(target);
+crystal1 = getRandomRange(1,12);
+crystal2 = getRandomRange(1,12);
+crystal3 = getRandomRange(1,12);
+crystal4 = getRandomRange(1,12);
+console.log( "c1 = " + crystal1 + " //  c2 = " + crystal2 + " //  c3 = " + crystal3 + " //  c4 = " + crystal4 + ".")
 
 }
 
@@ -24,3 +32,23 @@ crystal3 = 0;
 crystal4 = 0;
 chooseNumbers();
 }
+
+// random number range
+function getRandomRange(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+
+//   GAME PLAY_____________________________
+
+// if the user clicks the crystals, that amount is added to their score('mine')
+$('#c1').click(function() {
+    // mine += crystals amount
+    alert("hey steve");
+});
+
+alert("hey tim");
+
+// if the user score > target, loss; if the score = target, win
+
+// increment wins & losses
